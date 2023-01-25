50af5c196b6afa9801bf30f3194f42c4529f7bb0
Abhijit <abhijit.bhagavati1111@gmail.com>
2023-01-25 09:55:20 +0530

    added jenkinsfile to build the front and the backend in stages and store the artifacts

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,12 +1,18 @@
 package com.example.backend;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
